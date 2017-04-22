3d0edcb911aabd7ba6ae868c41bdb09bcf44df42
Khoi Phan <khoiphan21@gmail.com>
2017-04-22 17:17:41 +1000

    basic input layout done

--- src/app/input-display/input-display.component.ts
+++ src/app/input-display/input-display.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-display.component.scss']
 })
 export class InputDisplayComponent implements OnInit {
+  rotary: string = '100px';
+  weight: string = '100px';
+  diffPressure: string = '100px';
+  pumpOutput: string = '100px';
 
   constructor() { }
 
