f02311159c9e63ac36b49b289c74c8092c0c03d0
Andrea Yee <andrea_jy@hotmail.com>
2017-04-22 12:38:52 +1000

    updated referencing and styled the filter tabs

--- src/app/drill-img/drill-img.component.ts
+++ src/app/drill-img/drill-img.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-drill-img',
   templateUrl: './drill-img.component.html',
-  styleUrls: ['./drill-img.component.css']
+  styleUrls: ['./drill-img.component.scss']
 })
 export class DrillImgComponent implements OnInit {
 
